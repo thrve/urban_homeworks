01ad2295a00bbb90609340897bd43d90fc7c92f6
thrve <hithrv@yandex.ru>
2024-07-17 20:58:34 +0300

    change 2_3

--- urban/second_mod/module_2_3.py
+++ urban/second_mod/module_2_3.py
@@ -5,6 +5,6 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 list_index = 0
 
 
-while my_list[list_index] >= 0 and list_index < len(my_list) - 1:
+while my_list[list_index] > 0 and list_index < len(my_list) - 1:
     print(my_list[list_index])
     list_index += 1
