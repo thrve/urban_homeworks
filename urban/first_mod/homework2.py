13837ff15b7d37c2722d4be3df111c3707945c04
thrve <hithrv@yandex.ru>
2024-07-17 18:26:04 +0300

    plus homework2.py

---
+++ urban/first_mod/homework2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+
+total_tasks = 12
+hours_spent = 1.5
+average_completion_time = 0.125
+
+
+print(
+    f"Курс: Python, всего задач:{total_tasks}, затрачено часов: {hours_spent}, среднее время выполнения {average_completion_time} часа."
+)
--- urban/first_mod/variables.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-
-num1 = 5
-num2 = 3
-sum = num1 + num2
-print(sum)
-result = sum + num1 * num2
-print(result)
