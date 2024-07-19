79d4c217d414dbe53a70516dd7f17c39474a3e20
thrve <hithrv@yandex.ru>
2024-07-19 17:13:37 +0300

    plus tic-tac

--- urban/second_mod/tic-tac-toe.py
+++ urban/second_mod/tic-tac-toe.py
@@ -8,15 +8,31 @@ def draw_area():
     print()
 
 
+def turns(turn):
+    print(f"move: {turn}")
+    if turn % 2 == 0:
+        turn_char = "0"
+        print("the 0 move")
+    else:
+        turn_char = "X"
+        print("the X move")
+    row_column = input("select number row and column [1, 2, 3]: ")
+    print()
+    row_column_list = []
+    for i in row_column:
+        row_column_list.append(int(i) - 1)
+    arya[row_column_list[0]][row_column_list[1]] = turn_char
+
+
 arya = [["*", "*", "*"], ["*", "*", "*"], ["*", "*", "*"]]
 print("welcome to the tic-tac-toe")
 print("==========================\n")
 
 draw_area()
-row_column = input("select number row and column [1, 2, 3]: ")
-row_column_list = []
-for i in row_column:
-    row_column_list.append(int(i))
-print(row_column_list)
 
-draw_area()
+for turn in range(1, 10):
+    turns(turn)
+    if arya[row_column_list[0]][row_column_list[1]] == '*':
+        
+
+    draw_area()
