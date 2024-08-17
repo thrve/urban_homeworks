1d3238f5b0d5683da0d0d05279ab3710ad191443
thrve <hithrv@yandex.ru>
2024-08-17 05:52:22 +0300

    plus 5_1

--- urban/fifth_mod/module_5_1.py
+++ urban/fifth_mod/module_5_1.py
@@ -13,9 +13,3 @@ class House:
         else:
             # print(f'{i for i in range(1, new_floor)}')
             print(", ".join(str(i) for i in range(1, new_floor)))
-
-
-h1 = House("ЖК Горский", 18)
-h2 = House("Домик в деревне", 2)
-h1.go_to(5)
-h2.go_to(10)
