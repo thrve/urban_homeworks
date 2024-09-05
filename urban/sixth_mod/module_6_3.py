936c11de7daa133777172273fe58df5f592d46c0
thrve <hithrv@yandex.ru>
2024-09-05 05:48:53 +0300

    plus 6_3

---
+++ urban/sixth_mod/module_6_3.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+
+class Horse:
+    def __init__(self):
+        self.x_distance = (0,)
+        self.sound = "Frrr"
+
+    def run(self, dx):
+        self.x_distance += dx
+
+
+class Eagle:
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = "I train, eat, sleep, and repeat"
+
+    def fly(self, dy):
+        self.y_distance += dy
+
+
+class Pegasus(Horse, Eagle):
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
+
+    def move(self, dx, dy):
+        self.run(dx)
+        self.fly(dy)
+
+    def get_pos(self):
+        return (self.x_distance, self.y_distance)
+
+    def voice(self):
+        print(self.sound)
