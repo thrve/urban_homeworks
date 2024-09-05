0e90bb67050f55a9d9f2b3ec3a53ed6078d942d0
thrve <hithrv@yandex.ru>
2024-09-05 19:17:44 +0300

    + 6hard

--- urban/sixth_mod/module6hard.py
+++ urban/sixth_mod/module6hard.py
@@ -1,28 +1,69 @@
 #!/usr/bin/env python
 
 
+import math
+
+
 class Figure:
     sides_count = 0
 
-    def __init__(self, sides, color, filled=False):
-        self.__set_sides(sides)
-        self.__color = color
-        self.filled = filled
+    def __init__(self, color, *sides):
+        self.__sides = list(sides) if len(sides) == self.sides_count else [1] * self.sides_count
+        self.__color = list(color) if self.__is_valid_color(*color) else [0, 0, 0]
+        self.filled = False
 
-    def __set_sides(self, sides):
-        if isinstance(sides, list) and all(isinstance(side, int) for side in sides):
-            self.__sides = sides
-        else:
-            raise ValueError('the sides parameter expects a list of integers')
+    def __is_valid_color(self, r, g, b):
+        return all(isinstance(i, int) and 0 <= i <= 255 for i in (r, g, b))
+
+    def set_color(self, r, g, b):
+        if self.__is_valid_color(r, g, b):
+            self.__color = [r, g, b]
+
+    def get_color(self):
+        return self.__color
+
+    def __is_valid_sides(self, *new_sides):
+        return all(isinstance(side, int) and side > 0 for side in new_sides) and len(new_sides) == self.sides_count
+
+    def set_sides(self, *new_sides):
+        if self.__is_valid_sides(*new_sides):
+            self.__sides = list(new_sides)
+
+    def get_sides(self):
+        return self.__sides
+
+    def __len__(self):
+        return sum(self.__sides)
 
 
-class Circle:
-    pass
+class Circle(Figure):
+    sides_count = 1
+
+    def __init__(self, color, *sides):
+        super().__init__(color, *sides)
+        self.__radius = self.get_sides()[0] / (2 * math.pi)
+
+    def get_square(self):
+        return math.pi * self.__radius**2
 
 
-class Triangle:
-    pass
+class Triangle(Figure):
+    sides_count = 3
+
+    def get_square(self):
+        a, b, c = self.get_sides()
+        s = (a + b + c) / 2
+        return math.sqrt(s * (s - a) * (s - b) * (s - c))
 
 
-class Cube:
-    pass
+class Cube(Figure):
+    sides_count = 12
+
+    def __init__(self, color, *sides):
+        if len(sides) == 1:
+            sides = [sides[0]] * self.sides_count
+        super().__init__(color, *sides)
+
+    def get_volume(self):
+        side = self.get_sides()[0]
+        return side**3
