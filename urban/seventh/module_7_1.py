76a889fda385c807d22618206a10041dc715bdd2
thrve <hithrv@yandex.ru>
2024-09-06 21:49:50 +0300

    7_1 finished

--- urban/seventh/module_7_1.py
+++ urban/seventh/module_7_1.py
@@ -3,9 +3,29 @@
 
 class Product:
     def __init__(self, name, weight, categiry):
+        self.name = name
+        self.weight = weight
+        self.categiry = categiry
 
-    pass
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.categiry}'
 
 
 class Shop:
-    pass
+    __file_name = 'products.txt'
+
+    def get_products(self):
+        try:
+            with open(self.__file_name, 'r') as file:
+                return file.read()
+        except FileNotFoundError:
+            return ''
+
+    def add(self, *products):
+        existing_products = self.get_products()
+        with open(self.__file_name, 'a') as file:
+            for product in products:
+                if product.name not in existing_products:
+                    file.write(str(product) + '\n')
+                else:
+                    print(f'Product {product.name} is already in the store.')
