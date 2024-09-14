3f7a59b3d6dfa4a9ef3d07f675513096a9f25c21
thrve <hithrv@yandex.ru>
2024-09-14 05:06:14 +0300

    8_1

--- urban/eight_mod/module_8_1.py
+++ urban/eight_mod/module_8_1.py
@@ -6,9 +6,3 @@ def add_everything_up(a, b):
         return f'{a + b:.3f}'
     except TypeError:
         return str(a) + str(b)
-
-
-# Примеры использования функции
-print(add_everything_up(123.456, 'строка'))  # Вывод: 123.456строка
-print(add_everything_up('яблоко', 4215))  # Вывод: яблоко4215
-print(add_everything_up(123.456, 7))  # Вывод: 130.456
