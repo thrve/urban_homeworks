7ee809a092a25a30a99c50cfffc909886e129464
thrve <hithrv@yandex.ru>
2024-09-28 12:58:10 +0300

    9_7

---
+++ urban/ninth_mod/module_9_7.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+
+def is_prime(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        if result > 1:
+            for i in range(2, result):
+                if result % i == 0:
+                    print('Составное')
+                    break
+            else:
+                print('Простое')
+        else:
+            print('Составное')
+        return result
+
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+
+result = sum_three(2, 3, 6)
+print(result)
