e9b840ad297a61c854f02f2267ea6d179009399f
thrve <hithrv@yandex.ru>
2024-09-29 11:49:26 +0300

    change 9_6

--- urban/ninth_mod/module_9_6.py
+++ urban/ninth_mod/module_9_6.py
@@ -2,13 +2,7 @@
 
 
 def all_variants(text):
-    n = len(text)
-    total = n**2
-    for i in range(total):
-        variant = ''.join(text[j] for j in range(n) if (i & (1 << j)))
-        yield variant
-
-
-a = all_variants('abc')
-for i in a:
-    print(i)
+    length = len(text)
+    for i in range(1, length + 1):    
+        for j in range(0, length + 1 - i):  
+            yield text[j: j + i]
