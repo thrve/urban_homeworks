980410901253d24b55da8b3922e2b06f50e1849b
thrve <hithrv@yandex.ru>
2024-10-02 20:05:13 +0300

    10_3

---
+++ urban/tenth_mod/module_10_3.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+
+import threading
+import random
+import time
+
+
+class Bank:
+    def __init__(self):
+        self.balance = 0
+        self.lock = threading.Lock()
+
+    def deposit(self):
+        for _ in range(100):
+            amount = random.randint(50, 500)
+            with self.lock:
+                self.balance += amount
+                print(f'Пополнение: {amount}. Баланс: {self.balance}')
+                if self.balance >= 500 and self.lock.locked():
+                    self.lock.release()
+            time.sleep(0.001)
+
+    def take(self):
+        for _ in range(100):
+            amount = random.randint(50, 500)
+            self.lock.acquire()
+            try:
+                print(f'Запрос на {amount}')
+                if amount <= self.balance:
+                    self.balance -= amount
+                    print(f'Снятие: {amount}. Баланс: {self.balance}')
+                else:
+                    print('Запрос отклонён, недостаточно средств')
+            finally:
+                self.lock.release()
+            time.sleep(0.001)
+
+
+bk = Bank()
+
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
+
+th1.start()
+th2.start()
+
+th1.join()
+th2.join()
+
+print(f'Итоговый баланс: {bk.balance}')
