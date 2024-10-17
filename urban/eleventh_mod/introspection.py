9c096debc13a666fdadcbc471e4522f97e6df03b
thrve <hithrv@yandex.ru>
2024-10-17 07:02:23 +0300

    introspection

--- urban/eleventh_mod/introspection.py
+++ urban/eleventh_mod/introspection.py
@@ -11,13 +11,9 @@ class MyClass:
 
 def introspection_info(obj):
     obj_type = type(obj).__name__
-
     attributes = [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith('__')]
-
     methods = [method for method in dir(obj) if callable(getattr(obj, method)) and not method.startswith('__')]
-
     module = getattr(obj, '__module__', None)
-
     info = {
         'type': obj_type,
         'attributes': attributes,
