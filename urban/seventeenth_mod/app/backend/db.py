6f26d4e97a0637bcedaec0b53b915814439b8b1f
thrve <hithrv@yandex.ru>
2024-12-16 18:30:13 +0300

    + add

---
+++ urban/seventeenth_mod/app/backend/db.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+engine = create_engine('sqlite:///taskmanager.db', echo=True)
+
+SessionLocal = sessionmaker(bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
--- urban/seventeenth_mod/app/main.py
+++ urban/seventeenth_mod/app/main.py
@@ -2,14 +2,16 @@
 
 
 from fastapi import FastAPI
+
 from routers.tasks import router as tasks_router
 from routers.user import router as user_router
+from backend.db import Base, engine
+
 import uvicorn
 import os
 
 app = FastAPI()
-app.include_router(tasks_router)
-app.include_router(user_router)
+Base.metadata.create_all(bind=engine)
 
 ip_addr = os.environ['IP']
 
@@ -17,6 +19,10 @@ ip_addr = os.environ['IP']
 @app.get('/')
 def welcome():
     return {'message': 'Welcome to Taskmanager'}
+
+
+app.include_router(tasks_router)
+app.include_router(user_router)
 
 
 if __name__ == '__main__':
